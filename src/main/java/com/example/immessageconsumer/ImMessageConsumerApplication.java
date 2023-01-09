b23c4ffb71be33d9bba66196ce1e44562a8d4759
Wai Lwin Phyo <119388718+wailwinphyo2020@users.noreply.github.com>
2023-01-09 17:28:13 +0630

    init

---
+++ src/main/java/com/example/immessageconsumer/ImMessageConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.immessageconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImMessageConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImMessageConsumerApplication.class, args);
+    }
+
+}
